6f87302c7e624774e99cc9bde68bb9b03c7f7488
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-01 15:13:43 -0500

    sorta working

--- User.py
+++ User.py
@@ -1,15 +1,25 @@
 # your improved User class goes here
 class User:
-    posts = [] #????????
-    def __init__(self, name, email, license, age, gender):
+    all_posts = []
+    def __init__(self, name, age, gender, message = None):
         self.name = name
-        self.email = email
-        self.license = license
         self.age = age
         self.gender = gender
+        self.message = message
         
     def __str__(self):
         return f"My name is {self.name}. I am a {self.age} year old {self.gender} and my email address is {self.email}"
 
-    def __post__(self):
-        
+    def post(self, message):
+        new_post = {'name': self.name, 'message': message}
+        User.all_posts.append(new_post)
+
+#########  Driver code ############
+
+alice = User('Alice', '22', 'female')
+bob = User('Bob', '57', 'male')
+alice_post = alice.post('This is a test')
+bob_post = bob.post('This is my test')
+alice_post = alice.post('Did this work?')
+
+print(User.all_posts)     
