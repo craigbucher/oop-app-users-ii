b5d12e32366fbd4b83fdf7bfcd2c0c00c4b1bff4
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-01 13:23:04 -0500

    some code

--- User.py
+++ User.py
@@ -1,1 +1,15 @@
 # your improved User class goes here
+class User:
+    posts = [] #????????
+    def __init__(self, name, email, license, age, gender):
+        self.name = name
+        self.email = email
+        self.license = license
+        self.age = age
+        self.gender = gender
+        
+    def __str__(self):
+        return f"My name is {self.name}. I am a {self.age} year old {self.gender} and my email address is {self.email}"
+
+    def __post__(self):
+        
